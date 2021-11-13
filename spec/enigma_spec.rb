3433a8f593ee8a1cc81b77bce0172e78a5ab0f2d
Ruben Pasillas <hobbbes@protonmail.com>
2021-11-13 11:36:36 -0700

    typo

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -9,45 +9,52 @@ RSpec.describe Enigma do
     @enigma = Enigma.new
   end
 
-  it 'exists' do
-    expect(@enigma).to be_instance_of(Enigma)
+  context 'object initialization' do
+    it 'exists' do
+      expect(@enigma).to be_instance_of(Enigma)
+    end
+
+    it 'initializes empty char_set' do
+      expected_array = ["a".."z"].to_a << " "
+      expect(@enigma.char_set).to eq(expected_array)
+    end
+
+    it 'initializes empty keys' do
+      expect(@enigma.key).to eq(nil)
+    end
+
+    it 'initializes empty offsets' do
+      expect(@enigma.offsets).to eq(nil)
+    end
+
+    it 'initializes empty message' do
+      expect(@enigma.message).to eq(nil)
+    end
+
+    it 'initializes nil a..d keys via #key_setup?' do
+      expect(@enigma.key_setup?).to be true
+    end
+
+    it 'initializes nil a..d offsets via #offset_setup?' do
+      expect(@enigma.offset_setup?).to be true
+    end
   end
 
-  it 'initializes empty char_set' do
-    expected_array = ["a".."z"].to_a << " "
-    expect(@enigma.char_set).to eq(expected_array)
-  end
+  context 'object methods' do
+    it 'can store message via #encrypt' do
+      @enigma.encrypt("hello world", "02715", "111321")
+      expect(@enigma.message).to eq("hello world")
+    end
 
-  it 'initializes empty keys' do
-    expect(@enigma.key).to eq(nil)
-  end
+    it 'can store key via #encrypt' do
+      @enigma.encrypt("hello world", "02715", "111321")
+      expect(@enigma.key).to eq("02715")
+    end
 
-  it 'initializes empty offsets' do
-    expect(@enigma.offsets).to eq(nil)
-  end
-
-  it 'initializes empty message' do
-    expect(@enigma.message).to eq(nil)
-  end
-
-  it 'initializes nil a..d keys via #key_setup?' do
-    expect(@enigma.key_setup?).to be true
-  end
-
-  it 'initializes nil a..d offsets via #offset_setup?' do
-    expect(@enigma.offset_setup?).to be true
-  end
-
-  it 'can store message via #encrypt' do
-    expect(@enigma.message).to eq("hello world")
-  end
-
-  it 'can store key via #encrypt' do
-    expect(@enigma.key).to eq("02715")
-  end
-
-  it 'can store date via #encrypt' do
-    expect(@enigma.offset).to eq ("111321")
+    it 'can store date via #encrypt' do
+      @enigma.encrypt("hello world", "02715", "111321")
+      expect(@enigma.offsets).to eq ("111321")
+    end
   end
 
 end
