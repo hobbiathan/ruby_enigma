7535208af1529db100f996c352d74c1fab6c8ccc
Ruben Pasillas <hobbbes@protonmail.com>
2021-11-13 18:30:33 -0700

    refactor helper methods into new module

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,5 +1,7 @@
-class Enigma
+require_relative 'enigma_helpers'
 
+class Enigma
+  include EnigmaHelpers
 
   # TODO:
   # REFACTOR #ENCRYPT
@@ -18,38 +20,22 @@ class Enigma
     @offsets = nil
     @char_set = ("a".."z").to_a << " "
 
-    # lol
-    # keys_array[]
-    key_a = nil
-    key_b = nil
-    key_c = nil
-    key_d = nil
 
-    # offsets_array[]
-    offset_a = nil
-    offset_b = nil
-    offset_c = nil
-    offset_d = nil
-
-    # shifts_array[]
-    shift_a = nil
-    shift_b = nil
-    shift_c = nil
-    shift_d = nil
+    @keys_array = []
+    @offsets_array = []
+    @shifts_array = []
   end
 
-  # Checker methods
+  # Initialize checker methods
 
   # The first two just check to see if our attributes initialize as nil
   # Probably make into their own module later
   # Messy but better than creating 4 unnecessary attr_readers
 
-  # key_setup?(@keys_array)
+  # Not dry in the slightest
+  #
   def key_setup?
-    if @key_a == @key_b &&
-       @key_b == @key_c &&
-       @key_c == @key_d &&
-       @key_d == nil
+    if @keys_array == []
       true
     else
       false
@@ -59,10 +45,7 @@ class Enigma
   # if statement is pretty long
   # offset_setup?(@offsets_array)
   def offset_setup?
-    if @offset_a == @offset_b &&
-       @offset_b == @offset_c &&
-       @offset_c == @offset_d &&
-       @offset_d == nil
+    if @offsets_array == []
       true
     else
       false
@@ -71,80 +54,28 @@ class Enigma
 
   # shifts_setup?(@shifts_array)
   def shifts_setup?
-    if @shift_a == @shift_b &&
-       @shift_b == @shift_c &&
-       @shift_c == @shift_d &&
-       @shift_d == nil
-       true
-     else
-       false
-     end
-   end
+    if @shifts_array == []
+      true
+    else
+      false
+    end
+  end
 
   # Return our keys as an array to use as a check
   # for our #encrypt method functionality
+  # [keys_array[0], keys_array[1], etc...]
   def assign_key_check?
-    [@key_a, @key_b, @key_c, @key_d]
+    @keys_array[0..3]
   end
 
   # Same thing as method above, but for offset attributes
   def assign_offset_check?
-    [@offset_a, @offset_b, @offset_c, @offset_d]
+    @offsets_array[0..3]
   end
 
   def assign_shift_check?
-    [@shift_a, @shift_b, @shift_c, @shift_d]
+    @shifts_array[0..3]
   end
-
-  # Helper Methods (should be able to fit in a module)
-  # note : figure out a way later to make these return
-  # arrays containing variables with our appropriate
-  # values - will require arguments on #assign_shifts
-  def assign_keys(key)
-    @key_a = key[0..1]
-    @key_b = key[1..2]
-    @key_c = key[2..3]
-    @key_d = key[3..4]
-  end
-
-  def assign_offsets(date)
-    date_squared = date.to_i ** 2
-    # Not DRY, but I'll refactor it later
-    date_squared = date_squared.to_s
-
-    # require 'pry'; binding.pry
-    @offset_a = date_squared[-4].to_s
-    @offset_b = date_squared[-3].to_s
-    @offset_c = date_squared[-2].to_s
-    @offset_d = date_squared[-1].to_s
-  end
-
-  def assign_shifts
-    @shift_a = @key_a.to_i + @offset_a.to_i
-    @shift_b = @key_b.to_i + @offset_b.to_i
-    @shift_c = @key_c.to_i + @offset_c.to_i
-    @shift_d = @key_d.to_i + @offset_d.to_i
-
-    # Also not DRY for shit
-    @shift_a = @shift_a.to_s
-    @shift_b = @shift_b.to_s
-    @shift_c = @shift_c.to_s
-    @shift_d = @shift_d.to_s
-  end
-
-
-  # Module - #return_hash(encrypted_message, @key, @offsets)
-  def return_hash(encrypted_message)
-
-    enc_hash = {
-      encryption: encrypted_message,
-      key: @key,
-      date: @offsets
-
-    }
-
-  end
-
 
   # Actual Enigma functionality methods
 
@@ -155,9 +86,10 @@ class Enigma
       @key = key # Possible we don't even need this
       @offsets = date # Or this
 
-      assign_keys(key)
-      assign_offsets(date)
-      assign_shifts
+
+      @keys_array = assign_keys(key)
+      @offsets_array = assign_offsets(date)
+      @shifts_array = assign_shifts(@keys_array, @offsets_array)
 
     encrypted_message = []
 
@@ -171,7 +103,7 @@ class Enigma
         case shift_counter
         when 0
           relative_position = @char_set.index(char)
-          shifted = @char_set.rotate(@shift_a.to_i)
+          shifted = @char_set.rotate(@shifts_array[shift_counter])
 
           encrypted_message << shifted[relative_position]
 
@@ -180,7 +112,7 @@ class Enigma
 
         when 1
           relative_position = @char_set.index(char)
-          shifted = @char_set.rotate(@shift_b.to_i)
+          shifted = @char_set.rotate(@shifts_array[shift_counter])
 
           encrypted_message << shifted[relative_position]
 
@@ -190,7 +122,7 @@ class Enigma
 
         when 2
           relative_position = @char_set.index(char)
-          shifted = @char_set.rotate(@shift_c.to_i)
+          shifted = @char_set.rotate(@shifts_array[shift_counter])
 
           encrypted_message << shifted[relative_position]
 
@@ -199,7 +131,7 @@ class Enigma
 
         when 3
           relative_position = @char_set.index(char)
-          shifted = @char_set.rotate(@shift_d.to_i)
+          shifted = @char_set.rotate(@shifts_array[shift_counter])
 
           encrypted_message << shifted[relative_position]
 
@@ -212,7 +144,7 @@ class Enigma
       end
 
       return_hash(encrypted_message.join)
-
+      #require 'pry'; binding.pry
     end
 
 
