b344ab2a4916eaa6a47ada5fff163bfec06e5594
Ruben Pasillas <hobbbes@protonmail.com>
2021-11-13 19:06:04 -0700

    Rework return_hash

--- lib/enigma_helpers.rb
+++ lib/enigma_helpers.rb
@@ -1,10 +1,11 @@
 module EnigmaHelpers
 
+    # Returns 4 key values from originally provided key
     def assign_keys(key)
       [key[0..1], key[1..2], key[2..3], key[3..4]]
     end
 
-
+    # Gather last 4 integers of our squared date
     def assign_offsets(date)
       date_squared = date.to_i ** 2
       # Not DRY, but I'll refactor it later
@@ -13,6 +14,7 @@ module EnigmaHelpers
       [date_squared[-4], date_squared[-3], date_squared[-2], date_squared[-1]]
     end
 
+    # Shift values are just key + offset
     def assign_shifts(key_arr, offset_arr)
       [key_arr[0].to_i + offset_arr[0].to_i,
       key_arr[1].to_i + offset_arr[1].to_i,
@@ -20,16 +22,27 @@ module EnigmaHelpers
       key_arr[3].to_i + offset_arr[3].to_i]
     end
 
-
     # Module - #return_hash(encrypted_message, @key, @offsets)
-    def return_hash(encrypted_message)
+    def return_hash(message, key, date, state)
 
-      enc_hash = {
-        encryption: encrypted_message,
-        key: @key,
-        date: @offsets
+      if state == 'enc'
+        enc_hash = {
+          encryption: message,
+          key: key,
+          date: date
 
-      }
+        }
+
+      elsif state == 'dec'
+        dec_hash = {
+          decryption: message,
+          key: key,
+          date: date
+
+        }
+      else
+        puts "Invalid state."
+      end
 
     end
 end
