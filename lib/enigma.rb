a152754545c8dd3f12d869523f8c13cb4e0202e2
Ruben Pasillas <hobbbes@protonmail.com>
2021-11-13 19:06:14 -0700

    Complete overhaul

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,148 +4,133 @@ class Enigma
   include EnigmaHelpers
 
   # TODO:
-  # REFACTOR #ENCRYPT
+  # uhhh
+  # #decrypt
+  # #key_generator
+  # #date_generator
 
-  # (Possibly) move all keys/offset attributes into a module
-  # Move #key_setup?/#offset_setup? into helper modules
-  # Create #encrypt helper methods for autogenerating keys/date values
+  attr_reader :char_set
 
-
-  attr_reader :message, :key, :offsets, :char_set
-
-  # @offsets is the current date
   def initialize
-    @message = nil
-    @key = nil
-    @offsets = nil
     @char_set = ("a".."z").to_a << " "
-
-
-    @keys_array = []
-    @offsets_array = []
-    @shifts_array = []
   end
 
-  # Initialize checker methods
 
-  # The first two just check to see if our attributes initialize as nil
-  # Probably make into their own module later
-  # Messy but better than creating 4 unnecessary attr_readers
 
-  # Not dry in the slightest
-  #
-  def key_setup?
-    if @keys_array == []
-      true
-    else
-      false
-    end
-  end
 
-  # if statement is pretty long
-  # offset_setup?(@offsets_array)
-  def offset_setup?
-    if @offsets_array == []
-      true
-    else
-      false
-    end
-  end
-
-  # shifts_setup?(@shifts_array)
-  def shifts_setup?
-    if @shifts_array == []
-      true
-    else
-      false
-    end
-  end
-
-  # Return our keys as an array to use as a check
-  # for our #encrypt method functionality
-  # [keys_array[0], keys_array[1], etc...]
-  def assign_key_check?
-    @keys_array[0..3]
-  end
-
-  # Same thing as method above, but for offset attributes
-  def assign_offset_check?
-    @offsets_array[0..3]
-  end
-
-  def assign_shift_check?
-    @shifts_array[0..3]
-  end
-
-  # Actual Enigma functionality methods
-
-  # Elementary encrypt method, will figure out encryption
-  # first, then move to adding other required functional elements
   def encrypt(message, key, date)
-      @message = message.downcase
-      @key = key # Possible we don't even need this
-      @offsets = date # Or this
 
-
-      @keys_array = assign_keys(key)
-      @offsets_array = assign_offsets(date)
-      @shifts_array = assign_shifts(@keys_array, @offsets_array)
+    keys_array = assign_keys(key)
+    offsets_array = assign_offsets(date)
+    shifts_array = assign_shifts(keys_array, offsets_array)
 
     encrypted_message = []
 
-    message_as_array = message.split('')
+    message_as_array = message.downcase.split('')
 
-    # Definitely not DRY lol
-      shift_counter = 0
+  # Definitely not DRY lol
+  # Possibly make into its own method?
+    shift_counter = 0
 
-      message_as_array.each do |char|
+    message_as_array.each do |char|
 
-        case shift_counter
-        when 0
-          relative_position = @char_set.index(char)
-          shifted = @char_set.rotate(@shifts_array[shift_counter])
+      # Find position of current character in @char_set
+      relative_position = @char_set.index(char)
 
-          encrypted_message << shifted[relative_position]
+      case shift_counter
+      when 0
+        # Rotate @char_set by value of shifts_array[0]
+        shifted = @char_set.rotate(shifts_array[shift_counter])
+        # Shovel into encrypted_message array
+        encrypted_message << shifted[relative_position]
 
-          #require 'pry'; binding.pry
-          shift_counter += 1
+        shift_counter += 1
 
-        when 1
-          relative_position = @char_set.index(char)
-          shifted = @char_set.rotate(@shifts_array[shift_counter])
+      when 1
+        # Same as above, but with shifts_array[1]
+        shifted = @char_set.rotate(shifts_array[shift_counter])
+        encrypted_message << shifted[relative_position]
 
-          encrypted_message << shifted[relative_position]
+        shift_counter += 1
 
-          #require 'pry'; binding.pry
+      when 2
+        shifted = @char_set.rotate(shifts_array[shift_counter])
+        encrypted_message << shifted[relative_position]
 
-          shift_counter += 1
+        shift_counter += 1
 
-        when 2
-          relative_position = @char_set.index(char)
-          shifted = @char_set.rotate(@shifts_array[shift_counter])
+      when 3
+        shifted = @char_set.rotate(shifts_array[shift_counter])
+        encrypted_message << shifted[relative_position]
+        # Repeat this process until all characters have been gone through
+        shift_counter = 0
 
-          encrypted_message << shifted[relative_position]
-
-          shift_counter += 1
-
-
-        when 3
-          relative_position = @char_set.index(char)
-          shifted = @char_set.rotate(@shifts_array[shift_counter])
-
-          encrypted_message << shifted[relative_position]
-
-          shift_counter = 0
-
-
-        else
-          puts "lol"
-        end
+      else
+        puts "lol"
       end
-
-      return_hash(encrypted_message.join)
-      #require 'pry'; binding.pry
     end
 
+    return_hash(encrypted_message.join, key, date, 'enc')
+  end
+
+
+  def decrypt(message, key, date)
+
+    keys_array = assign_keys(key)
+    offsets_array = assign_offsets(date)
+    shifts_array = assign_shifts(keys_array, offsets_array)
+
+    decrypted_message = []
+
+    message_as_array = message.downcase.split('')
+
+  # Definitely not DRY lol
+  # Possibly make into its own method?
+    shift_counter = 0
+
+    message_as_array.each do |char|
+
+      # Find position of current character in @char_set
+      relative_position = @char_set.index(char)
+
+      case shift_counter
+      when 0
+        # Rotate @char_set by value of shifts_array[0]
+        shifted = @char_set.rotate(-shifts_array[shift_counter])
+        # Shovel into encrypted_message array
+        decrypted_message << shifted[relative_position]
+        #require 'pry'; binding.pry
+        shift_counter += 1
+
+      when 1
+        # Same as above, but with shifts_array[1]
+        shifted = @char_set.rotate(-shifts_array[shift_counter])
+        decrypted_message << shifted[relative_position]
+
+        shift_counter += 1
+
+      when 2
+        shifted = @char_set.rotate(-shifts_array[shift_counter])
+        decrypted_message << shifted[relative_position]
+
+        shift_counter += 1
+
+      when 3
+        shifted = @char_set.rotate(-shifts_array[shift_counter])
+        decrypted_message << shifted[relative_position]
+        # Repeat this process until all characters have been gone through
+        shift_counter = 0
+
+      else
+        puts "lol"
+      end
+    end
+
+    return_hash(decrypted_message.join, key, date, 'dec')
 
   end
+
+
+
+
+end
